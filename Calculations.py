897077bb7410f361d68cf7f7a1bd8c2153549441
JamieWatton32 <jamiewatton199559955@gmail.com>
2024-02-26 13:10:35 -0400

    doing stuff

--- Calculations.py
+++ Calculations.py
@@ -1,6 +1,6 @@
 #This file is responsible for the calculations from the written csv files. 
 #if the crawler hasn't been ran yet it will find it based on if the existance of ./CsvFiles returns true
-
+from datetime import datetime
 import numpy as np
 import pandas as pd
 import os
@@ -20,20 +20,45 @@ df2 = pd.read_csv(datafund_file)#Data fundamentals
 df3= pd.read_csv(web_file)#Web development
 df4 = pd.read_csv(prog_file)#Programming
 
-# Determines current grade in OSYS. 
-#TODO: SCRAPE QUIZ RESULTS NOW THAT BS4 IS MAKING IT FASTER
-def osys():
 
+#This function  
+def osys_quiz() -> float:
+    osys_quizzies =[pd.read_csv('./CsvFiles/OsysQuiz1.csv').to_numpy(),
+                    pd.read_csv('./CsvFiles/OsysQuiz2.csv').to_numpy(),
+                    pd.read_csv('./CsvFiles/OsysQuiz3.csv').to_numpy(),
+                    pd.read_csv('./CsvFiles/OsysQuiz4.csv').to_numpy(),
+                    pd.read_csv('./CsvFiles/OsysQuiz5.csv').to_numpy(),
+                    pd.read_csv('./CsvFiles/OsysQuiz6.csv').to_numpy(),
+                    pd.read_csv('./CsvFiles/OsysQuiz7.csv').to_numpy(),
+                    ]
+    if datetime.now() > datetime(2024,3,3,23,30):
+        osys_quizzies.append(pd.read_csv("./CsvFiles/OsysQuiz8.csv").to_numpy()) 
+
+    elif datetime.now() > datetime(2024,4,7,23,30):
+        osys_quizzies.append(pd.read_csv('./CsvFiles/OsysQuiz9.csv').to_numpy())
+
+    elif datetime.now() > datetime(2024,4,14,23,30):
+        osys_quizzies.append(pd.read_csv('./CsvFiles/OsysQuiz10.csv').to_numpy())
+    grades = []
+    total=int(0)
+
+    for i in osys_quizzies:
+        if i[-1][0] == 'Overall Grade (highest attempt):':
+            grades.append(float(i[-1][1])/50)
+    for j in grades:
+        total += j
+    return total
+
+def osys_grades() -> float:
     #these 3 lines are dropping empty rows, dropping duplicate rows, 
     #and converting it to a NumPy array for processing
     df1.dropna()
     df1.drop_duplicates()
     df_osys = df1.to_numpy()
-    #Empty lists for it. could probably be cleanmer but im bad at coding still so
     current = []
     total = []
     current_grade = {}
-    
+  
     #for osys he enters the stuff including the zero so no linear interpolation is needed. 
     for i in df_osys:
         if i[0] == "Assignments" or i[0] == "Quizes" or i[0] == "Final Project":
@@ -42,16 +67,73 @@ def osys():
     #Making the current grades into a dictionary for betteris readability.    
     current_grade = {
         "Assignments":float(current[0]),
-        "Quizzies":float(current[1]),
+        "Quizzies":osys_quiz(),
         "Final Project":float(current[2])
         }
-    Actual = 0#This is redundant on a processing level but helps the f string from being 8 billion characters in length.
-    Actual = current_grade['Assignments'] +current_grade["Final Project"] +current_grade["Quizzies"]
-    print(f'Your current grade in OSYS is {Actual}, however your quiz grades have not been posted so take that into account. ')
+    Actual = 0
+    Actual = current_grade['Assignments'] + current_grade["Final Project"] +current_grade["Quizzies"] 
+    print(f'Your current grade in OSYS is {Actual}')
 #put stuff here           
 def Networking():
-    pass
+    df_netw = df0.to_numpy()
+    netw_dict = {}
+    grades = []
+    current_grade_before = []
+    current_total=[]
+    assign_current = []
+    quiz_current = []
+    midterm_current = []
+    final_current =[]
 
+   
+    for i in df_netw:
+        if i[1] == "Weekly Assignments":# or i[1] != 'Weekly Quizzes': or i[1] !='Midterm Test' or i[1] !='Final Exam':
+            continue
+        elif i[1] == 'Weekly Quizzes':
+            continue
+        elif i[1] =='Midterm Test':
+            continue
+        elif i[1] =='Final Exam':
+            continue
+        else:
+            grades.append((i[2]).split('/'))
+    del grades[-1]        
+    for each in grades:
+        if each[0] == '- ':
+           each[0] = 0
+           each[1] = 0
+        current_grade_before.append(float(each[0]))
+        current_total.append(float(each[1]))
+    print(current_total)
+
+    
+    count = 0
+    for i in current_grade_before:
+        if count <= 5:
+            assign_current.append(i)
+        elif count > 5 and count <=11:
+            quiz_current.append(i)
+        elif count == 12:
+            midterm_current.append(i)
+        elif count == 13:
+            final_current.append(i)
+        count +=1
+    count = 0
+    for i in current_grade_before:
+        if count <= 5:
+            assign_current.append(i)
+        elif count > 5 and count <=11:
+            quiz_current.append(i)
+        elif count == 12:
+            midterm_current.append(i)
+        elif count == 13:
+            final_current.append(i)
+        count +=1
+
+    assign_worth_each = 8
+    for i in assign_current:
+        i = assign_worth_each*i
+    
 #put stuff here 
 def data_fundamentals():
     pass
@@ -63,7 +145,8 @@ def prog_logic():
     pass
 
 def main():
-    osys()
+    osys_grades()
+    Networking()
 
 if __name__ =="__main__":
         main()
