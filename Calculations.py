702bf41898a27fdb2340b0c09cf5666ebabfdc41
JamieWatton32 <jamiewatton199559955@gmail.com>
2024-02-27 10:18:58 -0400

    All classes output finished

--- Calculations.py
+++ Calculations.py
@@ -191,7 +191,15 @@ def data_fundamentals()-> float:
    
 #put stuff here 
 def web_dev()-> float:
-    pass
+    df_data = df3.to_numpy()
+    grades = []
+    for each in df_data:
+        if each[1] == "Assignments" or each[1] == "Practicals" or each[1] == "Final Project" or each[1] == 'Quizzes':
+            grades.append(each[2].split('/')[0])
+    sum_grades = 0
+    for i in grades:
+        sum_grades +=float(i)
+    return sum_grades
 #put stuff here 
 def prog_logic()-> float:
     df_prog = df4.to_numpy()
@@ -211,30 +219,31 @@ def main():
     print(f'Your current grade in networking is: {Networking():.2f}%')
     print(f'Your current grade in data fundamentals is: {data_fundamentals():.2f}%')
     print(f'Your current grade in programming and logic is: {prog_logic():.2f}%')
-    # #Removes files off your system if yes
-    # delete_files = input("Delete csv files?(y/n) ").lower()
-    # if delete_files == "y":
-    #     os.remove("./CsvFiles/Networking.csv")
-    #     os.remove("./CsvFiles/Osys.csv")
-    #     os.remove("./CsvFiles/DataFund.csv")
-    #     os.remove("./CsvFiles/Webdev.csv")
-    #     os.remove("./CsvFiles/Prog.csv")
-    #     os.remove("CsvFiles/OsysQuiz1.csv")
-    #     os.remove("CsvFiles/OsysQuiz2.csv")
-    #     os.remove("CsvFiles/OsysQuiz3.csv")
-    #     os.remove("CsvFiles/OsysQuiz4.csv")
-    #     os.remove("CsvFiles/OsysQuiz5.csv")
-    #     os.remove("CsvFiles/OsysQuiz6.csv")
-    #     os.remove("CsvFiles/OsysQuiz7.csv")
-    #     if datetime.now() > datetime(2024,3,3,23,30):
-    #         os.remove("./CsvFiles/OsysQuiz8.csv")
+    print(f'Your current grade in Web development is: {web_dev():.2f}%')
+    #Removes files off your system if yes
+    delete_files = input("Delete csv files?(y/n) ").lower()
+    if delete_files == "y":
+        os.remove("./CsvFiles/Networking.csv")
+        os.remove("./CsvFiles/Osys.csv")
+        os.remove("./CsvFiles/DataFund.csv")
+        os.remove("./CsvFiles/Webdev.csv")
+        os.remove("./CsvFiles/Prog.csv")
+        os.remove("CsvFiles/OsysQuiz1.csv")
+        os.remove("CsvFiles/OsysQuiz2.csv")
+        os.remove("CsvFiles/OsysQuiz3.csv")
+        os.remove("CsvFiles/OsysQuiz4.csv")
+        os.remove("CsvFiles/OsysQuiz5.csv")
+        os.remove("CsvFiles/OsysQuiz6.csv")
+        os.remove("CsvFiles/OsysQuiz7.csv")
+        if datetime.now() > datetime(2024,3,3,23,30):
+            os.remove("./CsvFiles/OsysQuiz8.csv")
 
-    #     elif datetime.now() > datetime(2024,4,7,23,30):
-    #         os.remove("./CsvFiles/OsysQuiz9.csv")
+        elif datetime.now() > datetime(2024,4,7,23,30):
+            os.remove("./CsvFiles/OsysQuiz9.csv")
 
-    #     elif datetime.now() > datetime(2024,4,14,23,30):
-    #         os.remove("./CsvFiles/OsysQuiz10.csv")
-    #     os.removedirs('./CsvFiles')
+        elif datetime.now() > datetime(2024,4,14,23,30):
+            os.remove("./CsvFiles/OsysQuiz10.csv")
+        os.removedirs('./CsvFiles')
 
 if __name__ =="__main__":
         main()
