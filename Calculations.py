cab65a717618b9c99ed4ed467923dcdb23a05d7a
JamieWatton32 <jamiewatton199559955@gmail.com>
2024-02-29 09:33:23 -0400

    final

--- Calculations.py
+++ Calculations.py
@@ -4,9 +4,10 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import os
+from Scraper import Scraper
 
 if os.path.isdir("./CsvFiles") == False:
-    import Scraper
+    Scraper()
 
 #Storing each file path as a string to help with readability
 networking_file = "./CsvFiles/Networking.csv"
